8196fea815fe0694a903a72d6cf2a0240e250be7
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2023-07-17 17:44:37 +0330

    for loop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { IfElseComponent } from './components/if-else/if-else.component';
+import { ForLoopComponent } from './components/for-loop/for-loop.component';
 
 const routes: Routes = [
   {path:'data-binding',component:DataBindingComponent},
   {path:'if-else',component:IfElseComponent},
+  {path:'for-loop',component:ForLoopComponent},
+
 
 ];
 
