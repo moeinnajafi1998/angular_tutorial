96705ef187114932dbdb893d2941ee5e37dcb583
moeinnajafi1998 <moeinnajafi1996@gmail.com>
2023-07-17 11:29:45 +0330

    if-else and ng-template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { IfElseComponent } from './components/if-else/if-else.component';
 
 const routes: Routes = [
   {path:'data-binding',component:DataBindingComponent},
+  {path:'if-else',component:IfElseComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
+import { IfElseComponent } from './components/if-else/if-else.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    IfElseComponent
   ],
   imports: [
     BrowserModule,
